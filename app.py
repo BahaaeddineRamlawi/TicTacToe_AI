12f3c63fd581f6b7ce7a3d5ad6e0b6ac563fef90
BahaaeddineRamlawi <bahaaeddineramlawi@gmail.com>
2025-01-05 21:47:10 +0200

    adding vercel stuff

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, render_template
+import os
 import random
-import json
 
 app = Flask(__name__)
 
@@ -228,5 +228,5 @@ def set_pruning():
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
